23d98b3572888eaeb294d729976f7e6877e27120
veudayab <veudayab@microsoft.com>
2014-04-02 11:10:19 -0700

    Version 3.1.0.1

--- Lib/WindowsPhone/Properties/AssemblyInfo.cs
+++ Lib/WindowsPhone/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.1.0.1")]
+[assembly: AssemblyFileVersion("3.1.0.1")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 #if SIGN
