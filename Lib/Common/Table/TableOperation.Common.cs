0afeef80a6aa0302e69a45970709859151cfc87e
Alberto Rodríguez <alberto.c.rodriguez@gmail.com>
2014-06-04 22:26:01 -0700

    Fixing TableOperation.Retrieve documentation.
    Should say retrieve, not replace.

--- Lib/Common/Table/TableOperation.Common.cs
+++ Lib/Common/Table/TableOperation.Common.cs
@@ -288,11 +288,11 @@ namespace Microsoft.WindowsAzure.Storage.Table
         }
 
         /// <summary>
-        /// Creates a new table operation that replaces the contents of
+        /// Creates a new table operation that retrieves the contents of
         /// the given entity in a table.
         /// </summary>
-        /// <param name="partitionKey">A string containing the partition key of the entity to be replaced.</param>
-        /// <param name="rowkey">A string containing the row key of the entity to be replaced.</param>
+        /// <param name="partitionKey">A string containing the partition key of the entity to retrieve.</param>
+        /// <param name="rowkey">A string containing the row key of the entity to retrieve.</param>
         /// <returns>The <see cref="TableOperation"/> object.</returns>
         [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "rowkey", Justification = "Reviewed : rowkey is allowed.")]
         public static TableOperation Retrieve(string partitionKey, string rowkey)
