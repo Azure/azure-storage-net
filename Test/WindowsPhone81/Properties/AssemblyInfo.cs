baff03ddfe8939b0c1a789e1ef1baeda22870426
pemari-msft <pemari@pemari-dev.redmond.corp.microsoft.com>
2015-09-29 17:24:15 -0700

    Storage Client Library 6.0.1-preview

--- Test/WindowsPhone81/Properties/AssemblyInfo.cs
+++ Test/WindowsPhone81/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyVersion("6.0.1.0")]
+[assembly: AssemblyFileVersion("6.0.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
