c867d4e26549d57544bd8c797669c75c65b022c4
jehine-MSFT <jehine@microsoft.com>
2015-12-10 14:04:02 -0800

    Storage Client Library 6.2.1

--- Lib/WindowsPhone/Properties/AssemblyInfo.cs
+++ Lib/WindowsPhone/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("6.2.0.0")]
-[assembly: AssemblyFileVersion("6.2.0.0")]
+[assembly: AssemblyVersion("6.2.1.0")]
+[assembly: AssemblyFileVersion("6.2.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 #if SIGN
