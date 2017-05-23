5a915d9d66fb81e9baeec06f1b5e0c4771cbf13d
asorrin-msft <asorrin@microsoft.com>
2017-05-23 10:54:48 -0700

    First pass at adding unit tests that double as API documentation. (#103)
    Documented Blob Request Options in detail, and UploadFromFile at a high level.

--- Lib/Common/Blob/BlobRequestOptions.cs
+++ Lib/Common/Blob/BlobRequestOptions.cs
@@ -243,6 +243,14 @@ namespace Microsoft.WindowsAzure.Storage.Blob
         /// Gets or sets the retry policy for the request.
         /// </summary>
         /// <value>An object of type <see cref="IRetryPolicy"/>.</value>
+        /// <remarks> Retry policies instruct the Storage Client to retry failed requests.
+        /// By default, only some failures are retried. For example, connection failures and 
+        /// throttling failures can be retried. Resource not found (404) or authentication 
+        /// failures are not retried, because these are not likely to succeed on retry.
+        /// If not set, the Storage Client uses an exponential backoff retry policy, where the wait time gets
+        /// exponentially longer between requests, up to a total of around 30 seconds.
+        /// The default retry policy is recommended for most scenarios.
+        ///</remarks>
         public IRetryPolicy RetryPolicy { get; set; }
 
 #if !(WINDOWS_RT || NETCORE)
@@ -256,6 +264,13 @@ namespace Microsoft.WindowsAzure.Storage.Blob
         /// Gets or sets a value to indicate whether data written and read by the client library should be encrypted.
         /// </summary>
         /// <value>Use <c>true</c> to specify that data should be encrypted/decrypted for all transactions; otherwise, <c>false</c>.</value>
+        /// <remarks>
+        /// RequireEncryption here refers to Client-Side Encryption.
+        /// If this value is set to <c>true</c>, all calls will fail if the data
+        /// is not encrypted/decrypted with an encryption policy. If this value 
+        /// is false (the default), any data being downloaded that is not encrypted
+        /// will be returned as-is.
+        /// </remarks>
         public bool? RequireEncryption { get; set; }
 
         /// <summary>
@@ -270,9 +285,23 @@ namespace Microsoft.WindowsAzure.Storage.Blob
         /// for the request. 
         /// </summary>
         /// <remarks>
-        /// This option is used only by the <see cref="CloudAppendBlob"/> object in the <b>UploadFrom*</b> methods and
-        /// the <b>BlobWriteStream</b> methods. By default, it is set to <c>false</c>. Set this option to <c>true</c> only for single writer scenarios.
+        /// This option is used only by the <see cref="CloudAppendBlob"/> object in the <b>UploadFrom*</b> methods, 
+        /// the <b>AppendFrom*</b> methods, and the <b>BlobWriteStream</b> class. By default, it is set to <c>false</c>. 
+        /// Set this option to <c>true</c> only for single-writer scenarios. 
         /// Setting this option to <c>true</c> in a multi-writer scenario may lead to corrupted blob data.
+        /// 
+        /// When calling "UploadFrom*" on an append blob, the Storage Client breaks the input data
+        /// up into a number of data blocks, and uploads each data block with an "append block" operation.
+        /// Normally, an "IfAppendPositionEqual" access condition is added to the append block operation, so that the
+        /// upload operation will fail if some other process somewhere has appended data in the middle of this data stream.
+        /// However, this can result in a false failure in a very specific case. If an append operation fails with a timeout,
+        /// there is a chance that the operation succeeded on the service, but the "success" response did not make it back to the client.
+        /// In this case, the client will retry, and then get an "append position not met" failure.
+        /// 
+        /// Setting this value to <c>true</c> results in the upload operation continuing when it sees an "append position not met"
+        /// failure on retry - accounting for the above possibility. However, this loses protection in the multi-writer
+        /// scenario - if multiple threads are uploading to the blob at once, and this value is set to <c>true</c>, some data
+        /// may be lost, because the client thinks the data was uploaded, when in fact it was the other process' data.
         /// </remarks>
         public bool? AbsorbConditionalErrorsOnRetry { get; set; }
 
@@ -280,18 +309,35 @@ namespace Microsoft.WindowsAzure.Storage.Blob
         /// Gets or sets the location mode of the request.
         /// </summary>
         /// <value>A <see cref="Microsoft.WindowsAzure.Storage.RetryPolicies.LocationMode"/> enumeration value indicating the location mode of the request.</value>
+        /// <remarks>The LocationMode specifies in which locations the Storage Client 
+        /// will attempt to make the request. This is only valid for RA-GRS accounts - accounts 
+        /// where data can be read from either the primary or the secondary endpoint.
+        ///</remarks>
         public LocationMode? LocationMode { get; set; }
 
         /// <summary>
-        /// Gets or sets the server timeout interval for the request.
+        /// Gets or sets the server timeout interval for a single HTTP request.
         /// </summary>
-        /// <value>A <see cref="TimeSpan"/> containing the server timeout interval for the request.</value>
+        /// <value>A <see cref="TimeSpan"/> containing the server timeout interval for each HTTP request.</value>
+        /// <remarks>
+        /// The server timeout is the timeout sent to the Azure Storage service 
+        /// for each REST request made. If the API called makes multiple REST calls 
+        /// (UploadFromStream, for example, or if the request retries), this timeout 
+        /// will be applied separately to each request. This value is not 
+        /// tracked or validated on the client, it is only passed to the Storage service.
+        /// </remarks>
         public TimeSpan? ServerTimeout { get; set; }
 
         /// <summary>
         /// Gets or sets the maximum execution time across all potential retries for the request. 
         /// </summary>
         /// <value>A <see cref="TimeSpan"/> representing the maximum execution time for retries for the request.</value>
+        /// <remarks>
+        /// The maximum execution time is the time allotted for a single API call.
+        /// If the total amount of time spent in the API - across all REST requests, 
+        /// retries, etc - exceeds this value, the client will timeout. This value 
+        /// is only tracked on the client, it is not sent to the service.
+        /// </remarks>
         public TimeSpan? MaximumExecutionTime
         {
             get
@@ -308,13 +354,19 @@ namespace Microsoft.WindowsAzure.Storage.Blob
 
                 this.maximumExecutionTime = value;
             }
-        }     
+        }
 
         /// <summary>
-        /// Gets or sets the number of blocks that may be simultaneously uploaded when uploading a blob that is greater than 
-        /// the value specified by the <see cref="SingleBlobUploadThresholdInBytes"/> property in size.
+        /// Gets or sets the number of blocks that may be simultaneously uploaded.
         /// </summary>
         /// <value>An integer value indicating the number of parallel blob upload operations that may proceed.</value>
+        /// <remarks>
+        /// When using the UploadFrom* methods on a blob, the blob will be broken up into blocks. Setting this 
+        /// value limits the number of outstanding I/O "put block" requests that the library will have in-flight 
+        /// at a given time. Default is 1 (no parallelism). Setting this value higher may result in 
+        /// faster blob uploads, depending on the network between the client and the Azure Storage service.
+        /// If blobs are small (less than 256 MB), keeping this value equal to 1 is advised.
+        ///</remarks>
         public int? ParallelOperationThreadCount
         {
             get
@@ -360,7 +412,17 @@ namespace Microsoft.WindowsAzure.Storage.Blob
         /// <summary>
         /// Gets or sets a value to calculate and send/validate content MD5 for transactions.
         /// </summary>
-        /// <value>Use <c>true</c> to calculate and send/validate content MD5 for transactions; otherwise, <c>false</c>.</value>       
+        /// <value>Use <c>true</c> to calculate and send/validate content MD5 for transactions; otherwise, <c>false</c>. Default is <c>false</c>.</value>
+        /// <remarks>
+        /// The UseTransactionalMD5 option instructs the Storage Client to calculate and validate 
+        /// the MD5 hash of individual Storage REST operations. For a given REST operation, 
+        /// if this value is set, both the Storage Client and the Storage service will calculate
+        /// the MD5 hash of the transferred data, and will fail if the values do not match.
+        /// This value is not persisted on the service or the client.
+        /// This option applies to both upload and download operations.
+        /// Note that HTTPS does a similar check during transit. If you are using HTTPS, 
+        /// we recommend this feature be off.
+        /// </remarks>
 #if  WINDOWS_PHONE && WINDOWS_DESKTOP
         /// <remarks>This property is not supported for Windows Phone.</remarks>
 #endif
@@ -388,8 +450,18 @@ namespace Microsoft.WindowsAzure.Storage.Blob
         /// <summary>
         /// Gets or sets a value to indicate that an MD5 hash will be calculated and stored when uploading a blob.
         /// </summary>
-        /// <value>Use <c>true</c> to calculate and store an MD5 hash when uploading a blob; otherwise, <c>false</c>.</value>
-        /// <remarks>This property is not supported for the <see cref="CloudAppendBlob"/> Append* APIs.</remarks>
+        /// <value>Use <c>true</c> to calculate and store an MD5 hash when uploading a blob; otherwise, <c>false</c>. Defaults to <c>false</c>.</value>
+        /// <remarks>This property is not supported for the <see cref="CloudAppendBlob"/> Append* APIs.
+        /// The StoreBlobContentMD5 request option instructs the Storage Client to calculate the MD5 hash 
+        /// of the blob content during an upload operation. This value is then stored on the 
+        /// blob object as the Content-MD5 header. This option applies only to upload operations. 
+        /// This is useful for validating the integrity of the blob upon later download, and 
+        /// compatible with the Content-MD5 header as defined in the HTTP spec. If using 
+        /// the Storage Client for later download, if the Content-MD5 header is present, 
+        /// the MD5 hash of the content will be validated, unless "DisableContentMD5Validation" is set.
+        /// Note that this value is not validated on the Azure Storage service on either upload or download of data; 
+        /// it is merely stored and returned.
+        /// </remarks>
 #if  WINDOWS_PHONE && WINDOWS_DESKTOP
         /// <remarks>This property is not supported for Windows Phone.</remarks>
 #endif
@@ -417,8 +489,14 @@ namespace Microsoft.WindowsAzure.Storage.Blob
         /// <summary>
         /// Gets or sets a value to indicate that MD5 validation will be disabled when downloading blobs.
         /// </summary>
-        /// <value>Use <c>true</c> to disable MD5 validation; <c>false</c> to enable MD5 validation.</value>
-#if  WINDOWS_PHONE && WINDOWS_DESKTOP
+        /// <value>Use <c>true</c> to disable MD5 validation; <c>false</c> to enable MD5 validation. Default is <c>false</c>.</value>
+        /// <remarks>
+        /// When downloading a blob, if the value already exists on the blob, the Storage service 
+        /// will include the MD5 hash of the entire blob as a header. This option controls 
+        /// whether or not the Storage Client will validate that MD5 hash on download.
+        /// See <see cref="BlobRequestOptions.StoreBlobContentMD5"/> for more details.
+        /// </remarks>
+#if WINDOWS_PHONE && WINDOWS_DESKTOP
         /// <remarks>This property is not supported for Windows Phone.</remarks>
 #endif
         public bool? DisableContentMD5Validation
