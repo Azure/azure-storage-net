d4ee18ffe3a0b5b018d0285a20be6bf6845ec0a2
Elham Rezvani <erezvani@microsoft.com>
2017-05-24 07:14:47 -0700

    update assembly versions to 8.1.3

--- Lib/Common/Shared/Protocol/Constants.cs
+++ Lib/Common/Shared/Protocol/Constants.cs
@@ -824,7 +824,7 @@ namespace Microsoft.WindowsAzure.Storage.Shared.Protocol
             /// <summary>
             /// Specifies the value to use for UserAgent header.
             /// </summary>
-            public const string UserAgentProductVersion = "8.1.2";
+            public const string UserAgentProductVersion = "8.1.3";
 
             /// <summary>
             /// Master Microsoft Azure Storage header prefix.
