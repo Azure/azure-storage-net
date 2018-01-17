8c0f291200996bfd6cf88006e7a7c140de6a67e3
erezvani <erezvani@microsoft.com>
2018-01-17 13:47:29 -0800

    [split] Update assembly version for release 8.7-prev

--- Test/ClassLibraryCommon/Blob/CloudBlobClientTest.cs
+++ Test/ClassLibraryCommon/Blob/CloudBlobClientTest.cs
@@ -688,9 +688,7 @@ namespace Microsoft.Azure.Storage.Blob
                 blobClient.GetContainerReference(containerName).Create();
             }
 
-            //            IEnumerable<CloudBlobContainer> results = blobClient.ListContainers();
-            string accountSAS = new CloudStorageAccount(blobClient.Credentials, false).GetSharedAccessSignature(new SharedAccessAccountPolicy() { Services = SharedAccessAccountServices.Blob, Permissions = SharedAccessAccountPermissions.Read | SharedAccessAccountPermissions.Write | SharedAccessAccountPermissions.List | SharedAccessAccountPermissions.Delete, SharedAccessExpiryTime = DateTime.Now + TimeSpan.FromDays(3), Protocols = SharedAccessProtocol.HttpsOrHttp, ResourceTypes = SharedAccessAccountResourceTypes.Service | SharedAccessAccountResourceTypes.Container | SharedAccessAccountResourceTypes.Object });
-            IEnumerable<CloudBlobContainer> results = new CloudStorageAccount(new StorageCredentials(accountSAS), "xclientdev2", "core.windows.net", false).CreateCloudBlobClient().ListContainers();
+            IEnumerable<CloudBlobContainer> results = blobClient.ListContainers();
 
 
             foreach (CloudBlobContainer container in results)
