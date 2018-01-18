1716adbbee8b82888fad2c0aef7644756ca64f6f
erezvani <erezvani@microsoft.com>
2018-01-18 00:30:07 -0800

    [split] blob: update build script for netstandard for new solution

--- NetstandardBuild/BuildNetStandardBlob.cmd
+++ NetstandardBuild/BuildNetStandardBlob.cmd
@@ -2,10 +2,10 @@ pushd %~dp0
 call ..\Tools\nuget3.4.exe install Microsoft.NETCore.Runtime.CoreCLR -Version 1.1.0
 cd ..\Lib\Facade.Split\Microsoft.Azure.Storage.Blob.Facade
 del project.lock.json
-call dotnet restore
+call dotnet restore --source ..\packages
 call dotnet build --configuration release
 cd ..\..\NetStandard.Split\Microsoft.Azure.Storage.Blob
 del project.lock.json
-call dotnet restore
+call dotnet restore --source ..\..\packages
 call dotnet build --configuration release
 popd
