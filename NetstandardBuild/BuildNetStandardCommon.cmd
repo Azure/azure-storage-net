9ee2d806c7d9e4bf1529e10b58c2316185e2044c
erezvani <erezvani@microsoft.com>
2018-01-18 10:29:40 -0800

    [split] fix build script for common package

--- NetstandardBuild/BuildNetStandardCommon.cmd
+++ NetstandardBuild/BuildNetStandardCommon.cmd
@@ -1,5 +1,5 @@
 pushd %~dp0
-call ..\..\Tools\nuget3.4.exe install Microsoft.NETCore.Runtime.CoreCLR -Version 1.1.0
+call ..\Tools\nuget3.4.exe install Microsoft.NETCore.Runtime.CoreCLR -Version 1.1.0
 cd ..\Lib\Facade.Split\Microsoft.Azure.Storage.Common.Facade
 del project.lock.json
 call dotnet restore
