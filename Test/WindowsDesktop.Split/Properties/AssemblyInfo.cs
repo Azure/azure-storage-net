1e57b6a18e318fb9c1cd3b9dd5c37e0e7006e70f
Keith Farmer <kfarmer@microsoft.com>
2018-11-09 12:55:40 -0800

    Update to v9.4.1

--- Test/WindowsDesktop.Split/Properties/AssemblyInfo.cs
+++ Test/WindowsDesktop.Split/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("9.4.0")]
-[assembly: AssemblyFileVersion("9.4.0")]
-[assembly: AssemblyInformationalVersion("9.4.0.0")]
+[assembly: AssemblyVersion("9.4.1")]
+[assembly: AssemblyFileVersion("9.4.1")]
+[assembly: AssemblyInformationalVersion("9.4.1.0")]
 
