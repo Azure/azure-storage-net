13129008b93a1ca4b0383459802c3d7160d97a98
Keith Farmer <kfarmer@microsoft.com>
2019-08-01 11:58:29 -0700

    Add WindowsForm test project for deadlock investigations

---
+++ WindowsFormsTest/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.blockUploadFromByteArray = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // blockUploadFromByteArray
+            // 
+            this.blockUploadFromByteArray.Location = new System.Drawing.Point(12, 12);
+            this.blockUploadFromByteArray.Name = "blockUploadFromByteArray";
+            this.blockUploadFromByteArray.Size = new System.Drawing.Size(406, 73);
+            this.blockUploadFromByteArray.TabIndex = 0;
+            this.blockUploadFromByteArray.Text = "CloudBlobBlob.UploadFromByteArray";
+            this.blockUploadFromByteArray.UseVisualStyleBackColor = true;
+            this.blockUploadFromByteArray.Click += new System.EventHandler(this.BlockUploadFromByteArray_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.blockUploadFromByteArray);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button blockUploadFromByteArray;
+    }
+}
+
---
+++ WindowsFormsTest/Form1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Auth;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.Storage.RetryPolicies;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsTest
+{
+    public partial class Form1 : Form
+    {
+        readonly TestConfigurations configurations;
+        readonly TenantConfiguration defaultTenantConfiguration;
+
+
+        private const AuthenticationScheme DefaultAuthenticationScheme = AuthenticationScheme.SharedKey;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                XElement element = XElement.Load(TestConfigurations.DefaultTestConfigFilePath);
+                this.configurations = TestConfigurations.ReadFromXml(element);
+
+                this.defaultTenantConfiguration = this.configurations.TenantConfigurations.Single(c => c.TenantName == this.configurations.TargetTenantName);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                throw new System.IO.FileNotFoundException("To run tests you need to supply a TestConfigurations.xml file with credentials in the Test/Common folder. Use TestConfigurationsTemplate.xml as a template.");
+            }
+        }
+
+        CloudBlobClient GenerateCloudBlobClient(DelegatingHandler delegatingHandler = null)
+        {
+            var storageCredentials = new StorageCredentials(this.defaultTenantConfiguration.AccountName, this.defaultTenantConfiguration.AccountKey);
+
+            CloudBlobClient client;
+            if (string.IsNullOrEmpty(this.defaultTenantConfiguration.BlobServiceSecondaryEndpoint))
+            {
+                Uri baseAddressUri = new Uri(this.defaultTenantConfiguration.BlobServiceEndpoint);
+                client = new CloudBlobClient(baseAddressUri, storageCredentials, delegatingHandler);
+            }
+            else
+            {
+                StorageUri baseAddressUri = new StorageUri(
+                    new Uri(this.defaultTenantConfiguration.BlobServiceEndpoint),
+                    new Uri(this.defaultTenantConfiguration.BlobServiceSecondaryEndpoint));
+                client = new CloudBlobClient(baseAddressUri, storageCredentials, delegatingHandler);
+            }
+
+            client.AuthenticationScheme = DefaultAuthenticationScheme;
+
+            return client;
+        }
+
+        private void BlockUploadFromByteArray_Click(object sender, EventArgs e)
+        {
+            CloudBlobClient client = this.GenerateCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference("windows-forms-upload-test-" + Guid.NewGuid().ToString());
+            container.CreateIfNotExists();
+            TimeSpan backoffPeriod = new TimeSpan();
+            try
+            {
+                BlobRequestOptions options = new BlobRequestOptions()
+                {
+                    SingleBlobUploadThresholdInBytes = 5 * 1024 * 1024,
+                    ParallelOperationThreadCount = 5,
+                    RetryPolicy = new ExponentialRetry(backoffPeriod, 2)
+                };
+                var bytes = new byte[40 * 1024 * 1024];
+                var cloudBlockBlob = container.GetBlockBlobReference("data.bin");
+                cloudBlockBlob.UploadFromByteArray(bytes, 0, bytes.Length, null, options, null);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            finally
+            {
+                container.DeleteIfExists();
+            }
+        }
+    }
+}
